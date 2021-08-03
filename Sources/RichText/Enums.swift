3b7a92ab591a80908751e8909345b01963c98446
LeeUngjae <73557895+NuPlay@users.noreply.github.com>
2021-08-03 17:01:10 +0900

    Add more option

---
+++ Sources/RichText/Enums.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 이웅재 on 2021/07/26.
+//
+
+import Foundation
+
+public enum colorScheme: String {
+    case light = "light"
+    case dark = "dark"
+    case automatic = "automatic"
+}
+
+public enum fontType : String {
+    case `default` = "default"
+    case monospaced = "monospaced"
+    case italic = "italic"
+}
--- Sources/RichText/RichText.swift
+++ Sources/RichText/RichText.swift
@@ -4,33 +4,49 @@ public struct RichText: View {
     @State private var dynamicHeight : CGFloat = .zero
     
     var html : String
+    
     var lineHeight : CGFloat = 170
     var imageRadius : CGFloat = 0
+    var fontType : fontType = .default
     
-    var colorScheme : colorScheme
-    
+    var colorScheme : colorScheme = .automatic
     var colorImportant : Bool = false
-    public init(html: String,lineHeight : CGFloat = 170, imageRadius : CGFloat = 0, colorScheme : colorScheme,colorImportant : Bool = false) {
+    
+    var placeholder: AnyView?
+    
+    public init(html: String,lineHeight : CGFloat = 170, imageRadius : CGFloat = 0, fontType : fontType = .default,colorScheme : colorScheme = .automatic, colorImportant : Bool = false) {
         self.html = html
         self.lineHeight = lineHeight
         self.imageRadius = imageRadius
+        self.fontType = fontType
         self.colorScheme = colorScheme
         self.colorImportant = colorImportant
     }
     
     public var body: some View {
-        Webview(dynamicHeight: $dynamicHeight, html: html, lineHeight: lineHeight, imageRadius: imageRadius,colorScheme: colorScheme,colorImportant: colorImportant)
-            .frame(height: dynamicHeight)
+        ZStack(alignment: .top){
+            Webview(dynamicHeight: $dynamicHeight, html: html, lineHeight: lineHeight, imageRadius: imageRadius,colorScheme: colorScheme,colorImportant: colorImportant)
+                .frame(height: dynamicHeight)
+            
+            if self.dynamicHeight == 0 {
+                placeholder
+            }
+        }
     }
 }
+
 
 struct RichText_Previews: PreviewProvider {
     static var previews: some View {
         RichText(html: "", colorScheme: .light)
     }
 }
+extension RichText {
+    public func placeholder<T>(@ViewBuilder content: () -> T) -> RichText where T : View {
+        var result = self
+        result.placeholder = AnyView(content())
 
-public enum colorScheme: String {
-    case light = "light"
-    case dark = "dark"
+        return result
+    }
 }
+
