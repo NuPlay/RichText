e4bce5196f0cc4b2691d38d9495dddfb09df389b
garam <studio@gar.am>
2023-07-14 18:49:19 +0900

    Update Color+Extension.swift

--- Sources/RichText/Extensions/Color+Extension.swift
+++ Sources/RichText/Extensions/Color+Extension.swift
@@ -51,11 +51,6 @@ extension NSColor {
         let r = Float(components[0])
         let g = Float(components[1])
         let b = Float(components[2])
-        var a = Float(1.0)
-
-        if components.count >= 4 {
-            a = Float(components[3])
-        }
 
         return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
     }
