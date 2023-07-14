2e9cfa2f27d57e2c9f30c2dca10fd186aabfb6f3
garam <studio@gar.am>
2023-07-14 15:53:16 +0900

    Update Color+Extension.swift

--- Sources/RichText/Extensions/Color+Extension.swift
+++ Sources/RichText/Extensions/Color+Extension.swift
@@ -6,89 +6,89 @@
 //
 
 #if canImport(UIKit)
-    import UIKit
+import UIKit
 
-    extension UIColor {
-        var hex: String? {
-            var red: CGFloat = 0
-            var green: CGFloat = 0
-            var blue: CGFloat = 0
-            var alpha: CGFloat = 0
+extension UIColor {
+    var hex: String? {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
 
-            let multiplier = CGFloat(255.999999)
+        let multiplier = CGFloat(255.999999)
 
-            guard getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
-                return nil
-            }
-
-            if alpha == 1.0 {
-                return String(
-                    format: "%02lX%02lX%02lX",
-                    Int(red * multiplier),
-                    Int(green * multiplier),
-                    Int(blue * multiplier)
-                )
-            } else {
-                return String(
-                    format: "%02lX%02lX%02lX%02lX",
-                    Int(red * multiplier),
-                    Int(green * multiplier),
-                    Int(blue * multiplier),
-                    Int(alpha * multiplier)
-                )
-            }
+        guard getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+            return nil
         }
-    }
-#else
-    import AppKit
 
-    extension NSColor {
-        convenience init(hex: String) {
-            let trimHex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-            let dropHash = String(trimHex.dropFirst()).trimmingCharacters(in: .whitespacesAndNewlines)
-            let hexString = trimHex.starts(with: "#") ? dropHash : trimHex
-            let ui64 = UInt64(hexString, radix: 16)
-            let value = ui64 != nil ? Int(ui64!) : 0
-            // #RRGGBB
-            var components = (
-                R: CGFloat((value >> 16) & 0xFF) / 255,
-                G: CGFloat((value >> 08) & 0xFF) / 255,
-                B: CGFloat((value >> 00) & 0xFF) / 255,
-                a: CGFloat(1)
+        if alpha == 1.0 {
+            return String(
+                format: "%02lX%02lX%02lX",
+                Int(red * multiplier),
+                Int(green * multiplier),
+                Int(blue * multiplier)
             )
-            if String(hexString).count == 8 {
-                // #RRGGBBAA
-                components = (
-                    R: CGFloat((value >> 24) & 0xFF) / 255,
-                    G: CGFloat((value >> 16) & 0xFF) / 255,
-                    B: CGFloat((value >> 08) & 0xFF) / 255,
-                    a: CGFloat((value >> 00) & 0xFF) / 255
-                )
-            }
-            self.init(red: components.R, green: components.G, blue: components.B, alpha: components.a)
-        }
-
-        func toHex(alpha: Bool = false) -> String? {
-            guard let components = cgColor.components, components.count >= 3 else {
-                return nil
-            }
-
-            let r = Float(components[0])
-            let g = Float(components[1])
-            let b = Float(components[2])
-            var a = Float(1.0)
-
-            if components.count >= 4 {
-                a = Float(components[3])
-            }
-
-            if alpha {
-                return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
-            } else {
-                return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
-            }
+        } else {
+            return String(
+                format: "%02lX%02lX%02lX%02lX",
+                Int(red * multiplier),
+                Int(green * multiplier),
+                Int(blue * multiplier),
+                Int(alpha * multiplier)
+            )
         }
     }
+}
+#else
+import AppKit
+
+extension NSColor {
+    convenience init(hex: String) {
+        let trimHex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        let dropHash = String(trimHex.dropFirst()).trimmingCharacters(in: .whitespacesAndNewlines)
+        let hexString = trimHex.starts(with: "#") ? dropHash : trimHex
+        let ui64 = UInt64(hexString, radix: 16)
+        let value = ui64 != nil ? Int(ui64!) : 0
+        // #RRGGBB
+        var components = (
+            R: CGFloat((value >> 16) & 0xFF) / 255,
+            G: CGFloat((value >> 08) & 0xFF) / 255,
+            B: CGFloat((value >> 00) & 0xFF) / 255,
+            a: CGFloat(1)
+        )
+        if String(hexString).count == 8 {
+            // #RRGGBBAA
+            components = (
+                R: CGFloat((value >> 24) & 0xFF) / 255,
+                G: CGFloat((value >> 16) & 0xFF) / 255,
+                B: CGFloat((value >> 08) & 0xFF) / 255,
+                a: CGFloat((value >> 00) & 0xFF) / 255
+            )
+        }
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: components.a)
+    }
+
+    func toHex(alpha: Bool = false) -> String? {
+        guard let components = cgColor.components, components.count >= 3 else {
+            return nil
+        }
+
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        var a = Float(1.0)
+
+        if components.count >= 4 {
+            a = Float(components[3])
+        }
+
+        if alpha {
+            return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+        } else {
+            return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        }
+    }
+}
 #endif
 
 extension Color {
