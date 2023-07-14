3b21fb14466a5e1ba8915482e3dada6111f3cb61
garam <studio@gar.am>
2023-07-14 15:42:14 +0900

    Add Color+Extension to support NSColor

---
+++ Sources/RichText/Extensions/Color+Extension.swift
@@ -0,0 +1,111 @@
+//
+//  Color+Extension.swift
+//
+//
+//  Created by Macbookator on 5.06.2022.
+//
+
+#if canImport(UIKit)
+    import UIKit
+
+    extension UIColor {
+        var hex: String? {
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            var alpha: CGFloat = 0
+
+            let multiplier = CGFloat(255.999999)
+
+            guard getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+                return nil
+            }
+
+            if alpha == 1.0 {
+                return String(
+                    format: "%02lX%02lX%02lX",
+                    Int(red * multiplier),
+                    Int(green * multiplier),
+                    Int(blue * multiplier)
+                )
+            } else {
+                return String(
+                    format: "%02lX%02lX%02lX%02lX",
+                    Int(red * multiplier),
+                    Int(green * multiplier),
+                    Int(blue * multiplier),
+                    Int(alpha * multiplier)
+                )
+            }
+        }
+    }
+#else
+    import AppKit
+
+    extension NSColor {
+        convenience init(hex: String) {
+            let trimHex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+            let dropHash = String(trimHex.dropFirst()).trimmingCharacters(in: .whitespacesAndNewlines)
+            let hexString = trimHex.starts(with: "#") ? dropHash : trimHex
+            let ui64 = UInt64(hexString, radix: 16)
+            let value = ui64 != nil ? Int(ui64!) : 0
+            // #RRGGBB
+            var components = (
+                R: CGFloat((value >> 16) & 0xFF) / 255,
+                G: CGFloat((value >> 08) & 0xFF) / 255,
+                B: CGFloat((value >> 00) & 0xFF) / 255,
+                a: CGFloat(1)
+            )
+            if String(hexString).count == 8 {
+                // #RRGGBBAA
+                components = (
+                    R: CGFloat((value >> 24) & 0xFF) / 255,
+                    G: CGFloat((value >> 16) & 0xFF) / 255,
+                    B: CGFloat((value >> 08) & 0xFF) / 255,
+                    a: CGFloat((value >> 00) & 0xFF) / 255
+                )
+            }
+            self.init(red: components.R, green: components.G, blue: components.B, alpha: components.a)
+        }
+
+        func toHex(alpha: Bool = false) -> String? {
+            guard let components = cgColor.components, components.count >= 3 else {
+                return nil
+            }
+
+            let r = Float(components[0])
+            let g = Float(components[1])
+            let b = Float(components[2])
+            var a = Float(1.0)
+
+            if components.count >= 4 {
+                a = Float(components[3])
+            }
+
+            if alpha {
+                return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+            } else {
+                return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+            }
+        }
+    }
+#endif
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = (rgbValue & 0xFF0000) >> 16
+        let g = (rgbValue & 0xFF00) >> 8
+        let b = rgbValue & 0xFF
+
+        self.init(
+            red: CGFloat(r) / 0xFF,
+            green: CGFloat(g) / 0xFF,
+            blue: CGFloat(b) / 0xFF, opacity: 1
+        )
+    }
+}
