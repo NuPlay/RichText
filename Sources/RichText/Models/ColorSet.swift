8ceaccbe7124edb66f1d6442a7314e37b730fe9d
garam <studio@gar.am>
2023-07-14 17:27:55 +0900

    Update ColorSet.swift

--- Sources/RichText/Models/ColorSet.swift
+++ Sources/RichText/Models/ColorSet.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 public struct ColorSet {
-    private let light: String
-    private let dark: String
+    private let light: Color
+    private let dark: Color
     public var isImportant: Bool
 
     public init(light: String, dark: String, isImportant: Bool = false) {
-        self.light = light
-        self.dark = dark
+        self.light = Color(hex: light)
+        self.dark = Color(hex: dark)
         self.isImportant = isImportant
     }
-    
-    public init(light: UIColor, dark: UIColor, isImportant: Bool = false) {
-        self.light = light.hex ?? "000000"
-        self.dark = dark.hex ?? "F2F2F2"
+
+    public init(light: Color, dark: Color, isImportant: Bool = false) {
+        self.light = light
+        self.dark = dark
         self.isImportant = isImportant
     }
     
